358015df6f710873b06f5e2c9689ab58513f8282
Neville Palmer <ntpalmer1@yahoo.co.uk>
2019-05-08 15:40:55 +0100

    Name

--- SecureRemote2/Form1.cs
+++ SecureRemote2/Form1.cs
@@ -116,7 +116,7 @@ namespace SecureRemote2
             MastercheckBox.Checked = true;
 
             //baseip = "172.16.199.0";
-            this.Text = "Network Assessor                   " + theVersion + "                               (c) 2019                    ";
+            this.Text = "Real Network Assessor                   " + theVersion + "                               (c) 2019                    ";
             try
             {
                 bool isExists = Directory.Exists(ConfigDir);
